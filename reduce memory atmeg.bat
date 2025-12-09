ef4b736bf528233b8d87ef23ad7ce4c6ced7dc21
Atmam <132759715+Atmeg23@users.noreply.github.com>
2025-12-09 21:04:25 +0700

    reduce memory atmeg.bat

--- reduce memory atmeg.bat
+++ reduce memory atmeg.bat
@@ -16,11 +16,6 @@ start /b "" EmptyStandbyList.exe workingsets
 start /b "" EmptyStandbyList.exe modifiedpagelist
 start /b "" EmptyStandbyList.exe standbylist
 
-:: Reset Pagefile (Opsional)
-wmic pagefile set InitialSize=0,MaximumSize=0 >nul 2>&1
-wmic pagefile delete >nul 2>&1
-wmic pagefile set InitialSize=8192,MaximumSize=8192 >nul 2>&1
-
 :: Hapus file sementara & Prefetch
 del /s /q %temp%\* >nul 2>&1
 del /s /q C:\Windows\Prefetch\* >nul 2>&1
@@ -32,3 +27,4 @@ sc stop WSearch >nul 2>&1
 
 :: Tutup CMD setelah selesai
 exit /b
+
